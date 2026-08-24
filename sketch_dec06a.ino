d50a58291a43821d6b42be8eaa9d83e568d1d637
Tanmoy Sural <tanmoysural1209@gmail.com>
2026-08-24 21:45:45 +0530

    Add files via upload

---
+++ sketch_dec06a.ino
@@ -0,0 +1,14 @@
+int a=12;
+int b=15;
+int c=18;
+void loop()
+{
+ if {
+  (a>b)(a>c); 
+ Serial.print("grade C");
+ Serial.print(a);
+ } else {
+    (b>a)(b>c);
+  Serial.print("grade B");
+  Serial.print1n(b);
+}}
